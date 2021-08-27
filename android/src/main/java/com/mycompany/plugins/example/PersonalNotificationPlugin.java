dc8588ef71875e5347436354b9d265ee6c950568
stevensurjadi1101 <stevensurjadi1101@gmail.com>
2021-08-27 15:57:41 +0700

    first commit

--- android/src/main/java/com/mycompany/plugins/example/PersonalNotificationPlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/PersonalNotificationPlugin.java
@@ -1,8 +1,33 @@
 package com.mycompany.plugins.example;
 
-public class PersonalNotificationPlugin {
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PermissionState;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
 
-    public String echo(String value) {
-        return value;
+@CapacitorPlugin(
+  name = "Notification",
+  permissions = {
+    @Permission(
+      alias = "notifications",
+      strings = {}
+    )
+  }
+)
+public class NotificationPlugin extends Plugin {
+
+  @PluginMethod()
+  public void getNotification(PluginCall call) {
+    if (getPermissionState("notifications") == PermissionState.GRANTED) {
+      JSObject ret = new JSObject();
+      ret.put("value", "getNotification");
+      notifyListeners("notificationEvent", ret);
+
+    } else {
+      call.reject("Enable notification is required");
     }
+  }
 }
